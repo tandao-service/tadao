d3321a21889a81ed60c652893a20e1e75fb1f598
Paul Irungu <paul.irungu@gmail.com>
2025-03-15 09:55:31 +0300

    first commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,18 +1,6 @@
-import Footer from "@/components/shared/Footer";
-//import Header from "@/components/shared/Header";
-import { getUserById } from "@/lib/actions/user.actions";
-//import { auth } from "@clerk/nextjs";
-import { Toaster } from "@/components/ui/toaster";
-//import { UpdateUserParams } from "@/types";
 import { auth } from "@clerk/nextjs/server";
 import { SignedIn } from "@clerk/nextjs";
-import Image from "next/image";
-import BottomNavigation from "@/components/shared/BottomNavigation";
-import Navbarhome from "@/components/shared/navbarhome";
-import ClientFCMHandler from "@/components/shared/ClientFCMHandler";
-import Head from "next/head";
 import ReceiveChat from "@/components/shared/ReceiveChat";
-
 export default async function RootLayout({
   children,
 }: {
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,35 +1,10 @@
-import dynamic from "next/dynamic";
-import Image from "next/image";
-import Skeleton from "@mui/material/Skeleton";
 import { SearchParamProps } from "@/types";
 import { auth } from "@clerk/nextjs/server";
-import { SignedIn, SignedOut } from "@clerk/nextjs";
-import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
-import Link from "next/link";
-import { getAllAd } from "@/lib/actions/ad.actions";
-import MenuSubmobile from "@/components/shared/MenuSubmobile";
-//import Collection from "@/components/shared/Collection";
-import { createUser } from "@/lib/actions/user.actions";
-import { getfcmTokenFromCookie } from "@/lib/actions/cookies";
-import CollectionInfinite from "@/components/shared/CollectionInfinite";
-import AppPopup from "@/components/shared/AppPopup ";
-import Footer from "@/components/shared/Footer";
-//import Header from "@/components/shared/Header";
 import { getUserById } from "@/lib/actions/user.actions";
-//import { auth } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
-
-//import { UpdateUserParams } from "@/types";
-import BottomNavigation from "@/components/shared/BottomNavigation";
-import Navbarhome from "@/components/shared/navbarhome";
-import ClientFCMHandler from "@/components/shared/ClientFCMHandler";
-import Head from "next/head";
 import { getAllCategories } from "@/lib/actions/category.actions";
 import { getAllSubCategories } from "@/lib/actions/subcategory.actions";
 import { getAdsCountAllRegion } from "@/lib/actions/dynamicAd.actions";
-import PropertyMap from "@/components/shared/PropertyMap";
-import GoogleMapp from "@/components/shared/GoogleMapping";
-import HomePage from "@/components/shared/HomePage";
 import MainPage from "@/components/shared/MainPage";
 
 export default async function Home({ searchParams }: SearchParamProps) {
