67e8a0512a2356e17b22e90a2d4f82980dcde83c
Paul Irungu <paul.irungu@gmail.com>
2025-10-07 09:24:14 +0300

    first commit

--- components/shared/HomeClient.tsx
+++ components/shared/HomeClient.tsx
@@ -6,6 +6,7 @@ import { getUserByClerkId } from "@/lib/actions/user.actions";
 import HomeDashboard from "@/components/shared/HomeDashboard";
 import { useAuth } from "@/app/hooks/useAuth";
 import { useRouter } from "next/navigation";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function HomeClient(props: any) {
     const { user, loading: authLoading } = useAuth();
@@ -32,7 +33,9 @@ export default function HomeClient(props: any) {
     }, [user]);
 
     if (authLoading) {
-        return <div>Loading...</div>;
+        return <div className="flex justify-center items-center h-full text-lg font-bold">
+            <div className="flex gap-2 items-center">  <CircularProgress sx={{ color: "gray" }} size={30} /> <div className="hidden lg:inline">Loading...</div></div>
+        </div>;
     }
 
     return (
