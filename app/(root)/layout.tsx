d68139fd34db9a1fbc4b763b27f8b83f8b7478a2
Paul Irungu <paul.irungu@gmail.com>
2026-03-10 08:58:10 +0300

    first commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,10 +3,6 @@ import TopProgressBar from "@/components/home/TopProgressBar";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex-1 overflow-x-hidden">
-<<<<<<< HEAD
-      <TopProgressBar height={3} colorClassName="bg-orange-500" />
-=======
->>>>>>> 874a1dfc95576a63fe83d03a1675763967327409
       {children}
     </main>
   );
