29397b84fffa3999af72006ca00409c8e736cd3d
Paul Irungu <paul.irungu@gmail.com>
2026-03-10 09:17:35 +0300

    first commit

--- components/home/HomePage.tsx
+++ components/home/HomePage.tsx
@@ -1,18 +1,12 @@
 // components/home/HomePage.tsx
-<<<<<<< HEAD
-=======
 import { Suspense } from "react";
->>>>>>> 874a1dfc95576a63fe83d03a1675763967327409
 import type { HomeAd, HomeCategory, HomeRegion } from "@/lib/home/home.data";
 import { HomeCategoryNode } from "@/lib/home/home.categories";
 import HomeShell from "./HomeShell.client";
 
-<<<<<<< HEAD
-=======
 function HomeShellFallback() {
     return <div>Loading...</div>;
 }
->>>>>>> 874a1dfc95576a63fe83d03a1675763967327409
 
 export default function HomePage({
     categories,
@@ -28,15 +22,6 @@ export default function HomePage({
     categoryTree: HomeCategoryNode[];
 }) {
     return (
-<<<<<<< HEAD
-        <HomeShell
-            categories={categories}
-            featured={featured}
-            trending={trending}
-            regions={regions}
-            categoryTree={categoryTree}
-        />
-=======
         <Suspense fallback={<HomeShellFallback />}>
             <HomeShell
                 categories={categories}
@@ -46,6 +31,5 @@ export default function HomePage({
                 categoryTree={categoryTree}
             />
         </Suspense>
->>>>>>> 874a1dfc95576a63fe83d03a1675763967327409
     );
 }
